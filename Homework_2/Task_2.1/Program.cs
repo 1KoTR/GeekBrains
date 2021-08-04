790e5f28a6e8df18fa70b8d0b66ebc15ef76b0d5
1KoTR <denis.kotr@gmail.com>
2021-08-05 02:42:12 +0300

    Перегруппировал задачи. Начал делать второе дз.

---
+++ Homework_2/Task_2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/* Чечулин Денис
+ *  
+ * 1. 
+ * 
+ * Написать метод, возвращающий минимальное из трёх чисел.
+ * 
+ */
+
+namespace Task_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.23;
+            double b = 12332;
+            double c = -1;
+
+            Console.WriteLine($"Числа: {a} | {b} | {c}");
+            Console.WriteLine($"Минимальное: {Min(a, b, c)}");
+        }
+
+        static double Min(double a, double b, double c)
+        {
+            if (a <= b && a <= c)
+                return a;
+            if (b <= a && b <= c)
+                return b;
+            return c;
+        }
+    }
+}
---
+++ Homework_2/Task_2.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/* Чечулин Денис
+ *  
+ * 2. 
+ * 
+ * Написать метод подсчета количества цифр числа.
+ * 
+ */
+
+namespace Task_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = 901230.000400012019m;
+
+            Console.WriteLine($"Число: {a}");
+            Console.WriteLine($"Его длина: {NumberLenght(a)}");
+        }
+
+        static int NumberLenght(decimal number)
+        {
+            int numberLenght = 0;
+
+            long longNumber = (long)number;
+
+            // Выносит цифры числа за запятую. Делает из decimal - long
+            while (longNumber != number)
+            {
+                number *= 10;
+                longNumber = (long)number;
+            }
+
+            // Считает количество цифр в числе.
+            while (longNumber > 0)
+            {
+                longNumber /= 10;
+                numberLenght++;
+            }
+
+            return numberLenght;
+        }
+    }
+}
