72df98b3609ca0d8485a3f0842a54f4dad33fc4e
1KoTR <denis.kotr@gmail.com>
2021-08-26 22:11:29 +0300

    Начал восьмое дз.

---
+++ Homework_8/Task_8.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/* Чечулин Денис
+ *  
+ * 1. 
+ * 
+ * С помощью рефлексии выведите все свойства структуры DateTime.
+ * 
+ */
+
+namespace Task_8._1
+{
+    interface IReflection
+    {
+        void Serialize();
+        void Deserialize();
+    }
+
+    class Reflection : IReflection
+    {
+        public void Serialize()
+        {
+
+        }
+
+        public void Deserialize()
+        {
+            throw new Exception("Не лезь сюда!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
